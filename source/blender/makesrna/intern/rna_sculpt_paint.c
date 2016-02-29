3d87b960896bd6592b5ce990f0c52e7a981abced
Joshua Leung <aligorith@gmail.com>
2016-02-29 16:58:00 +1300

    PSculpt: Reduce maximum possible size of brushes

--- source/blender/makesrna/intern/rna_sculpt_paint.c
+++ source/blender/makesrna/intern/rna_sculpt_paint.c
@@ -1147,7 +1147,7 @@ static void rna_def_pose_sculpt(BlenderRNA *brna)
 	RNA_def_struct_ui_text(srna, "Pose Sculpt Brush", "Pose sculpting brush");
 
 	prop = RNA_def_property(srna, "size", PROP_INT, PROP_NONE);
-	RNA_def_property_range(prop, 1, SHRT_MAX);
+	RNA_def_property_range(prop, 1, MAX_BRUSH_PIXEL_RADIUS);
 	RNA_def_property_ui_range(prop, 1, 300, 10, 3);
 	RNA_def_property_ui_text(prop, "Radius", "Radius of the brush in pixels");
 	RNA_def_property_update(prop, 0, "rna_PSculptBrush_update");
